a6d0297277790413a8bf904776f08cbf3bd14c8b
ilexwg <ilexwg@gmail.com>
2018-12-20 16:15:10 +0800

    add v-header component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
+import VHeader from 'components/v-header/v-header'
+import { getSeller } from 'api'
+import axios from 'axios'
+
 export default {
   name: 'app',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    // axios.get('/api/seller').then((data) => {
+    //   console.log(data)
+    // })
+    getSeller().then((seller) => {
+      this.seller = seller
+    })
+  },
   components: {
+    VHeader
   }
 }
 </script>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
+<style lang="stylus" scoped>
+
 </style>
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import { get } from './helpers'
+
+const getSeller = get('/api/seller')
+
+export { getSeller }
